9622fc8a924047693879bd0ff67114e4fd526791
sergiocouter <sergiocouter@gmail.com>
2023-01-22 11:06:52 -0300

    adicionado script js

---
+++ javascript/formulario.js
@@ -0,0 +1,57 @@
+var formulario = document.querySelector("#form");
+            var nome = document.querySelector("#nome");
+            var email = document.querySelector("#email");
+            var mediaDosPesos = document.querySelector("#media_dos_pesos");
+
+        document.getElementById("btnSubmit").onclick = function(e) {
+            
+            var radios1 = document.getElementsByName("primeiraPergunta");
+
+            for (var i = 0; i < radios1.length; i++) {
+                if (radios1[i].checked) {
+                    console.log(nome.value);
+                    console.log(email.value);
+                    console.log("Peso Pergunta 1: " + radios1[i].value);
+                    var peso1 = radios1[i].value
+                    
+                }
+            }
+
+            var radios2 = document.getElementsByName("segundaPergunta");
+
+            for (var i = 0; i < radios2.length; i++) {
+                if (radios2[i].checked) {
+                    console.log("Peso Pergunta 2: " + radios2[i].value);
+                    var peso2 = radios2[i].value;
+
+                }
+            }
+
+            var radios3 = document.getElementsByName("terceiraPergunta");
+
+            for (var i = 0; i < radios3.length; i++) {
+                if (radios3[i].checked) {   
+                    console.log("Peso Pergunta 3: " + radios3[i].value);
+                    var peso3 = radios3[i].value;
+
+                }
+            }
+
+            var radios4 = document.getElementsByName("quartaPergunta");
+
+            for (var i = 0; i < radios4.length; i++) {
+                if (radios4[i].checked) {
+                    console.log("Peso Pergunta 4: " + radios4[i].value);
+                    var peso4 = radios4[i].value;
+
+                }
+            }
+
+            var media = (parseFloat(peso1) + parseFloat(peso2) + parseFloat(peso3) + parseFloat(peso4)) / 4;
+               console.log("A media dos pesos é: " + media);
+            
+
+            mediaDosPesos.value = media;
+            console.log(mediaDosPesos.value);
+            
+        };
